a1ce672c68f426b160b946cb133f3301c8475a4c
Solarpasserby <yuruizhang188@gmail.com>
2024-12-06 22:57:07 +0800

    lab4 3.1

--- lab4/src/pipe.c
+++ lab4/src/pipe.c
@@ -156,7 +156,6 @@ void pipe_stage_mem()
             else {
                 /* cache miss */
                 pipe.data_cache_miss_stall = CACHE_MISS_STALL;
-                free(op);
                 return;
             }
         }
