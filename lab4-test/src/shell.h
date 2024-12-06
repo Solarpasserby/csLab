d3ef59e979f3e7597483afe0309e7707448c094e
Solarpasserby <yuruizhang188@gmail.com>
2024-12-06 23:13:34 +0800

    cmp with the correct answer

---
+++ lab4-test/src/shell.h
@@ -0,0 +1,42 @@
+/***************************************************************/
+/*                                                             */
+/*   MIPS-32 Instruction Level Simulator                       */
+/*                                                             */
+/*   Computer Architecture - Professor Onur Mutlu              */
+/*                                                             */
+/***************************************************************/
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+/*          DO NOT MODIFY THIS FILE!                            */
+/*          You should only change sim.c!                       */
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+
+#ifndef _SIM_SHELL_H_
+#define _SIM_SHELL_H_
+
+#include <stdint.h>
+
+#define FALSE 0
+#define TRUE  1
+
+#define MEM_DATA_START  0x10000000
+#define MEM_DATA_SIZE   0x00100000
+#define MEM_TEXT_START  0x00400000
+#define MEM_TEXT_SIZE   0x00100000
+#define MEM_STACK_START 0x7ff00000
+#define MEM_STACK_SIZE  0x00100000
+#define MEM_KDATA_START 0x90000000
+#define MEM_KDATA_SIZE  0x00100000
+#define MEM_KTEXT_START 0x80000000
+#define MEM_KTEXT_SIZE  0x00100000
+
+extern int RUN_BIT;	/* run bit */
+
+/* only the cache touches these functions */
+uint32_t mem_read_32(uint32_t address);
+void     mem_write_32(uint32_t address, uint32_t value);
+
+/* statistics */
+extern uint32_t stat_cycles, stat_inst_retire, stat_inst_fetch, stat_squash;
+
+#endif
